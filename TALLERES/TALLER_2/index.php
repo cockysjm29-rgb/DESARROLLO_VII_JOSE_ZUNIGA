aada317a989b25dd7fd5ee8ec3ff6daec4499e1f
Jose <cockysjm@gmail.com>
2025-08-25 19:10:53 -0500

    Agregada concatenación de variables y constantes

---
+++ TALLERES/TALLER_2/index.php
@@ -0,0 +1,21 @@
+<?php
+$nombre = "Juan";
+$edad = 25;
+
+// Concatenación usando el operador .
+$presentacion1 = "Hola, mi nombre es " . $nombre . " y tengo " . $edad . " años.";
+
+// Concatenación dentro de comillas dobles
+$presentacion2 = "Hola, mi nombre es $nombre y tengo $edad años.";
+
+// Definición de una constante
+define("SALUDO", "¡Bienvenido!");
+
+// Concatenación con constante
+$mensaje = SALUDO . " " . $nombre;
+
+echo $presentacion1 . "<br>";
+echo $presentacion2 . "<br>";
+echo $mensaje . "<br>";
+
+?>
