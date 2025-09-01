9c62aab2443d15b932b72dbdc779c15ab85e8e3e
Jose <cockysjm@gmail.com>
2025-09-01 18:25:09 -0500

    Añadido ejercicio de str_replace()

--- TALLERES/TALLER_3/str_replace.php
+++ TALLERES/TALLER_3/str_replace.php
@@ -8,8 +8,8 @@ echo "Frase modificada: $fraseModificada</br>";
 
 // Ejercicio: Crea una variable con una frase que contenga al menos tres veces la palabra "PHP"
 // y usa str_replace() para cambiar "PHP" por "JavaScript"
-$miFrase = ""; // Reemplaza esto con tu frase
-$miFraseModificada = str_replace("PHP", "JavaScript", $miFrase);
+$miFrase = "El vaso debe estar con mucho hielo"; // Reemplaza esto con tu frase
+$miFraseModificada = str_replace("mucho", "poco", $miFrase);
 
 echo "</br>Mi frase original: $miFrase</br>";
 echo "Mi frase modificada: $miFraseModificada</br>";
